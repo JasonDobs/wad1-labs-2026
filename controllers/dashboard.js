8ec7138ad1f6d171279690e60c7dac56ecf4db34
Jason Roy Dambra O'Brien <Factioniwillafk@outlook.com>
2026-02-10 13:05:55 +0000

    Add Models and views.

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,30 +2,15 @@
 
 import logger from "../utils/logger.js";
 
+const dashboard = {
+    createView(request, response) {
+        logger.info("Dashboard page loading!");
 
-const playlist = [
-    {
-        id: 1,
-        title: "Piano Sonata No. 3",
-        artist: "Beethoven"
-    },
-    {
-        id: 2,
-        title: "Piano Sonata No. 7",
-        artist: "Beethoven"
-    },
-    {
-        id: 3,
-        title: "Piano Sonata No. 10",
-        artist: "Beethoven"
-    }
-];
+        const viewData = {
+            title: "Playlist App Dashboard"
+        };
 
-const dashboard ={
-    createView(request,response){
-        logger.info("Dashboard page loading!")
-        logger.debug("Loading the playlist", playlist);
-        response.json(playlist);
+        response.render('dashboard', viewData);
     },
 };
 
---
+++ models/card.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import logger from '../utils/logger.js';
+import JsonStore from "./json-store.js";
+const cardStore = {
+
+    store: new JsonStore('./models/card.json',{employee: {} }),
+    collection: 'employee',
+
+
+    getAppInfo() {
+        return this.store.findAll(this.collection);
+    },
+
+};
+export default cardStore;
